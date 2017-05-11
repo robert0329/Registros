0892c83cf99920ec786568fae7959f2f85d14fde
robert antonio frias de leon <chico_3_13@hotmail.com>
2017-05-11 15:21:05 -0400

    yinaaa

---
+++ Registros/DAL/BeautyBaseDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Registros.DAL
+{
+    public class BeautyBaseDB : DbContext
+    {
+
+        public BeautyBaseDB() : base("ConStr")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Registros.Models.Clientes> Clientes { get; set; }
+
+        public System.Data.Entity.DbSet<Registros.Models.Empledos> Empledos { get; set; }
+    }
+}
---
+++ Registros/Models/Clientes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Registros.Models
+{
+    public class Clientes
+    {
+        [Key]
+
+        public int ClienteID { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Email { get; set; }
+        public string Cedula { get; set; }
+    }
+}
