c8a5a965be5d66c05a799c2c76d57b2ea292bda8
robert antonio frias de leon <chico_3_13@hotmail.com>
2017-05-13 18:44:04 -0400

    Validando

--- Registros/Models/Clientes.cs
+++ Registros/Models/Clientes.cs
@@ -11,11 +11,27 @@ namespace Registros.Models
         [Key]
 
         public int ClienteID { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un nombre")]
+        [StringLength(50)]
+        [RegularExpression(@"^[zA-Z]([a-zA-Z])")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar una telefono")]
+        [StringLength(100)]
         public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar una direccion")]
+        [StringLength(100)]
         public string Direccion { get; set; }
+
         public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar un email")]
+        [StringLength(100)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar una cedula")]
+        [StringLength(100)]
         public string Cedula { get; set; }
     }
 }
