0bdca49174831734dd6ed63cd021abfbe1c7df17
robert antonio frias de leon <chico_3_13@hotmail.com>
2017-05-13 23:47:19 -0400

    validacion empleados 90%

--- Registros/Models/Empleados.cs
+++ Registros/Models/Empleados.cs
@@ -10,14 +10,41 @@ namespace Registros.Models
     {
         [Key]
         public int EmpleadosID { get; set; }
+
+        [StringLength(100)]
+        [RegularExpression("^([a-zA-Z .&']+)$", ErrorMessage = "Solo se puede escribir letras")]
+        [Required(ErrorMessage = "Debe ingresar un nombre")]
         public string Nombre { get; set; }
-        public string Cedula { get; set; }
-        public string Telefono { get; set; }
-        public string Direccion { get; set; }
-        public DateTime FechaNacimiento { get; set; }
+        
         public DateTime FechaDeEntrada { get; set; }
-        public double SueldoFijo { get; set; }
+
+        [StringLength(100)]
+        [RegularExpression("^([0-9]+)$", ErrorMessage = "Solo se puede escribir numeros")]
+        [Required(ErrorMessage = "Debe ingresar un sueldo")]
+        public string SueldoFijo { get; set; }
+        
+
+        [StringLength(100)]
+        [RegularExpression("^([0-9.&-]+)$", ErrorMessage = "Solo se puede escribir numeros")]
+        [Required(ErrorMessage = "Debe ingresar una telefono")]
+        public string Telefono { get; set; }
+
+        [StringLength(100)]
+        [RegularExpression("^([a-zA-Z0-9 .&'-]+)$", ErrorMessage = "Contiene caracteres Invalidos")]
+        [Required(ErrorMessage = "Debe ingresar una direccion")]
+        public string Direccion { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        [StringLength(100)]
+        [RegularExpression("^([a-zA-Z0-9.&'@.&'-]+)$", ErrorMessage = "Contiene caracteres Invalidos")]
+        [Required(ErrorMessage = "Debe ingresar un email")]
         public string Email { get; set; }
+
+        [StringLength(100)]
+        [RegularExpression("^([0-9.&-]+)$", ErrorMessage = "Solo se puede escribir numeros")]
+        [Required(ErrorMessage = "Debe ingresar una cedula")]
+        public string Cedula { get; set; }
     }
 
 }
